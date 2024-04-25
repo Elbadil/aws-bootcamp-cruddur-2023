5bcac522bed66392a5818777d8105f8c27360329
Elbadil <elbaliadil@gmail.com>
2024-04-25 18:20:09 +0100

    Added CloudWatch Logs And X-RAY to the backend app

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,6 +22,15 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+# X-Ray -------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+# CloudWatch Logs ------
+import watchtower
+import logging
+from time import strftime
+
 
 # HoneyComb -----
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -31,13 +40,26 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
-
 app = Flask(__name__)
+
+# X-Ray -------
+# xray_url = os.getenv("AWS_XRAY_URL")
+# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+# XRayMiddleware(app, xray_recorder)
 
 # HoneyComb -----
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
+
+# Configuring Logger to Use CloudWatch
+# LOGGER = logging.getLogger(__name__)
+# LOGGER.setLevel(logging.DEBUG)
+# console_handler = logging.StreamHandler()
+# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+# LOGGER.addHandler(console_handler)
+# LOGGER.addHandler(cw_handler)
+# LOGGER.info('Test log')
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -49,6 +71,12 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+# @app.after_request
+# def after_request(response):
+#   timestamp = strftime('[%Y-%b-%d %H:%M]')
+#   LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#   return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -90,7 +118,7 @@ def data_home():
   data = HomeActivities.run()
   return data, 200
 
-
+# Added notifications endpoint
 @app.route("/api/activities/notifications", methods=['GET'])
 def data_activity():
   data = NotificationsActivities.run()
